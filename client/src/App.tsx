196acfa8963f3dc9d9d6cdf78a24e98de88f7ae2
Simas <simas@simas-dei.com>
2020-02-15 16:52:21 +0200

    add API call

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//@ts-nocheck
+import React, { useEffect, useState } from 'react';
+
+import { getNumberArray } from './api';
+import './app.css';
+
+import { ArrayItems } from './components';
 
 const App = () => {
+  const [arrayItems, setArrayItems] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const result = await getNumberArray();
+      if (result) setArrayItems(result);
+    })();
+  }, []);
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
+    const {
+      target: { value },
+    } = e;
+    setArrayItems(state => {
+      state[index] = +value;
+      return state;
+    });
+    console.log(value, arrayItems);
+  };
+
+  if (!arrayItems.length) return <div className='App'>Loading...</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <ArrayItems arrayItems={arrayItems} onChangeHandler={onChangeHandler} />
     </div>
   );
-}
+};
 
 export default App;
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 
 const algoRouter = require('./routes/algoRoutes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/v1/algo', algoRouter);
 
 const port = 5000;
-app.listen(port);
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
